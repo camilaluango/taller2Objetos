bc1f2956b8b13ef4046b50fbdf7846925a8cc3a7
camila <mariacamilaluango@gmail.com>
2025-02-21 19:50:40 -0500

    array

--- scrript.js
+++ scrript.js
@@ -1,4 +1,10 @@
 const perfumeria = {
-    
+    perfumes : [{nombre:"sauvage", precio:800, cantidad:24},
+        {nombre:"miss dior", precio:80, cantidad:12},
+        {nombre:"yara", precio:600, cantidad:24},
+        {nombre:"360", precio:400, cantidad:15},
+        {nombre:"ultra male", precio:1000, cantidad:28}
+    ],
+
 };
 
